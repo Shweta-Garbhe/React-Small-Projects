faeadbe8db4ba8101d0a4c425795e3f8f2d87412
Shweta-Adya <50661151+Shweta-Garbhe@users.noreply.github.com>
2023-06-02 16:01:05 +0530

    fetch and show data on button click

---
+++ FetchAndShow/Fetch and show data from API.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [users, setUsers] = useState(null);
+
+  const fetchUserData = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=10");
+    const data = await response.json();
+    setUsers(data.results);
+  };
+
+  const handleClick = () => {
+    fetchUserData();
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        Find a list of random users below fetched from random API on button
+        click{" "}
+      </h1>
+      <button onClick={() => handleClick()}>Get details</button>
+      {users && (
+        <table>
+          <thead>
+            <tr>
+              <td>
+                <b>First name</b>
+              </td>
+              <td>
+                <b>Last name</b>
+              </td>
+              <td>
+                <b>Email</b>
+              </td>
+              <td>
+                <b>Contact Number</b>
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user, index) => (
+              <tr key={index}>
+                <td>{user?.name?.first}</td>
+                <td>{user?.name?.last}</td>
+                <td>{user?.email}</td>
+                <td>{user?.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
