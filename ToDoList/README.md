b0341d7193210957781770a8586ae1c8654a743e
Shweta-Adya <50661151+Shweta-Garbhe@users.noreply.github.com>
2023-07-06 08:01:37 +0530

    To Do List added

---
+++ ToDoList/README.md
@@ -0,0 +1,10 @@
+# To do List
+
+- Implemented a To Do list, where number of tasks are shown with checkbox
+- User can check on checkboxes then that task will be marked as completed and a line through will appear on it's text.
+- If user unchecks any tasks, the task will be marked as not completed and line through will be removed from it's text.
+- The most updated task is shown on bottom to highlight which task user is updating.
+
+### Solution image
+
+![Solution Snap For Reference](./public/solution.jpg)
---
+++ ToDoList/src/App.js
@@ -0,0 +1,48 @@
+const { useState } = React;
+
+function TodoApp() {
+  const [items, setItems] = useState([
+    { id: 1, text: "Learn JavaScript", done: false },
+    { id: 2, text: "Learn React", done: false },
+    { id: 3, text: "Play around in JSFiddle", done: true },
+    { id: 4, text: "Build something awesome", done: true },
+  ]);
+
+  const [updatedItem, setUpdatedItem] = useState("");
+
+  const handleChange = (id) => {
+    const updatedItems = [...items];
+
+    const updatedIndex = updatedItems.findIndex((item) => item.id === id);
+
+    if (updatedItems[updatedIndex].id === id) {
+      updatedItems[updatedIndex].done = !updatedItems[updatedIndex].done;
+      setItems(updatedItems);
+      setUpdatedItem(updatedItems[updatedIndex].text);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Todos:</h2>
+      <ol>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={item.done}
+                onChange={() => handleChange(item.id)}
+              />
+              <span className={item.done ? "done" : ""}>{item.text}</span>
+            </label>
+          </li>
+        ))}
+      </ol>
+      <br />
+      <p>{updatedItem && "Updated Item: " + updatedItem}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById("app"));
